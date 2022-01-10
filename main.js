829f8a510615d562ce8b3ade0f8f08a06fb9274f
DanieleFic <reb_ficus22@hotmail.it>
2022-01-10 16:49:51 +0100

    commenti

--- main.js
+++ main.js
@@ -2,44 +2,42 @@ const team = [
   {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-    image: 'wayne-barnett-founder-ceo.jpg',
+    image: 'img/wayne-barnett-founder-ceo.jpg',
   },
   {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-    image: 'angela-caroll-chief-editor.jpg',
+    image: 'img/angela-caroll-chief-editor.jpg',
   },
   {
     name: 'Walter Gordon',
     role: 'Office Manager',
-    image: 'walter-gordon-office-manager.jpg',
+    image: 'img/walter-gordon-office-manager.jpg',
   },
   {
     name: 'Angela Lopez',
     role: 'Social Media Manager',
-    image: 'angela-lopez-social-media-manager.jpg',
+    image: 'img/angela-lopez-social-media-manager.jpg',
   },
   {
     name: 'Scott Estrada',
     role: 'Developer',
-    image: 'scott-estrada-developer.jpg',
+    image: 'img/scott-estrada-developer.jpg',
   },
   {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-    image: 'barbara-ramos-graphic-designer.jpg',
+    image: 'img/barbara-ramos-graphic-designer.jpg',
   },
 ];
 
-
-
-
+//facciamo una funziona che crea le card e le mette nel Dom
 function creazioneCard( card ){
   document.getElementById("container").innerHTML +=
   `<div class="team-card">
     <div class="card-image">
       <img
-        src="img/${card.image}"
+        src="${card.image}"
         alt=""
       />
       </div>
@@ -51,9 +49,49 @@ function creazioneCard( card ){
       </div>
     `
 }
-
+//ciclo for che crea carte in base alla lunghezza dell array degli ogetti
 for(let i=0; i < team.length; i++){
   creazioneCard( team[i] )
 }
+
+//variabile che prende in considerazione il buttone 
+let inserisciPersona = document.getElementById("addMemberButton");
+
+
+//Funzione che ti crea le nuove card in base a quello che scrivi nei campi del DOM
+function creazioneCard2(){
+  let nome = document.getElementById("name").value;
+  let role = document.getElementById("role").value;
+  let image = document.getElementById("image").value;
+  
+  let userCard = {
+    name : nome,
+    role : role,
+    image : image,
+  }
+  
+  document.getElementById("container").innerHTML +=
+  `<div class="team-card">
+    <div class="card-image">
+      <img
+        src="${userCard.image}"
+        alt="${userCard.nome}"
+      />
+      </div>
+      <div class="card-text">
+        <h3>${userCard.nome}</h3>
+        <p>${userCard.role}</p>
+        </div>
+      </div>
+      </div>
+    `
+    //Pusha il nuovo oggetto nell array
+    team.push(userCard)
+    console.log(team)
+}
+
+//funzione che al click ti crea le card
+inserisciPersona.addEventListener("click", creazioneCard2);
+
 
 
